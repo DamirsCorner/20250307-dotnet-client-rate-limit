5977c91d95f21ae32997717d4a41ec7ca96ff59a
Damir Arh <damir.arh@gmail.com>
2025-03-01 12:19:43 +0100

    Add project files.

---
+++ ClientSideRateLimiting/RateLimitingClient.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.RateLimiting;
+using Polly;
+
+namespace ClientSideRateLimiting
+{
+    internal class RateLimitingClient
+    {
+        private readonly HttpClient client;
+        private readonly string url;
+        private readonly ResiliencePipeline pipeline;
+
+        public RateLimitingClient(string url, int maxRequestsPerSecond)
+        {
+            client = new HttpClient();
+            this.url = url;
+            pipeline = new ResiliencePipelineBuilder()
+                .AddRateLimiter(
+                    new SlidingWindowRateLimiter(
+                        new SlidingWindowRateLimiterOptions
+                        {
+                            PermitLimit = maxRequestsPerSecond,
+                            SegmentsPerWindow = 10,
+                            Window = TimeSpan.FromSeconds(1)
+                        }
+                    )
+                )
+                .Build();
+        }
+
+        public async Task<string> GetAsync()
+        {
+            return await pipeline.ExecuteAsync(async cancellationToken =>
+                await client.GetStringAsync(url, cancellationToken)
+            );
+        }
+    }
+}
---
+++ ClientSideRateLimiting/RateLimitingClientTests.cs
@@ -0,0 +1,43 @@
+﻿using Polly.RateLimiting;
+using Shouldly;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace ClientSideRateLimiting
+{
+    public class RateLimitingClientTests
+    {
+        private WireMockServer server;
+
+        [SetUp]
+        public void Setup()
+        {
+            server = WireMockServer.Start();
+            server
+                .Given(Request.Create().WithPath("/").UsingGet())
+                .RespondWith(Response.Create().WithBody("Hello world!"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            server.Stop();
+            server.Dispose();
+        }
+
+        [Test]
+        public async Task BlocksCallsExceedingRateLimit()
+        {
+            var client = new RateLimitingClient(server.Urls[0], 5);
+
+            var tasks = Enumerable.Range(0, 5).Select(_ => client.GetAsync()).ToArray();
+            await Task.WhenAll(tasks).ShouldNotThrowAsync();
+
+            await client.GetAsync().ShouldThrowAsync<RateLimiterRejectedException>();
+
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            await client.GetAsync().ShouldNotThrowAsync();
+        }
+    }
+}
